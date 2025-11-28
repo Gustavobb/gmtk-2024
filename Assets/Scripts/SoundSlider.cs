94cf8f04084d06283e79971ff1c4711aa1800872
mikomoares <mihanoch@gmail.com>
2025-11-28 16:49:24 -0300

    Audio Slider

---
+++ Assets/Scripts/SoundSlider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slider_music : MonoBehaviour
+{
+    private Slider slider;
+    public GameObject source;
+    public bool isMusic;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = PlayerPrefs.GetFloat(isMusic ? "musicVolume" : "SFXVolume", .5f);
+
+        if (slider != null)
+        {
+            slider.onValueChanged.AddListener(value => source.GetComponent<SFXControls>().AdjustVol(slider.value));
+        }
+        else
+        {
+            Debug.LogError("erro");
+        }
+    }
+}
